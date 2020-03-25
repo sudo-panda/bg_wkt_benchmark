8c4d8cf0f98dcb882007fd1033beca74bd8a7282
Baidyanath Kundu <kundubaidya99@gmail.com>
2020-03-25 16:52:43 +0530

    Add script to generate results and result directory

---
+++ gen_result.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+DIR=$(pwd)
+if [ ! -d "results" ] 
+then
+	echo "Result directory does not exist. Creating ..."
+	mkdir results
+else
+	echo "Result directory exists"
+fi
+if [ ! -d "build" ] 
+then
+	echo "Build directory does not exist. Creating ..."
+	mkdir build
+else
+	echo "Build directory exists"
+fi
+cd build
+echo "Running cmake and make in $(pwd)"
+cmake ..
+make
+./benchmark_bg_wkt_o2 -t $DIR/results/benchmark_bg_wkt_o2.csv
+./benchmark_bg_wkt_o3 -t $DIR/results/benchmark_bg_wkt_o3.csv
