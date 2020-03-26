9b511a8cc3a6c046e2a7b9687ce64f1e52461370
Baidyanath Kundu <kundubaidya99@gmail.com>
2020-03-26 23:43:31 +0530

    Update results

--- gen_result.sh
+++ gen_result.sh
@@ -1,23 +1,49 @@
 #!/bin/bash
 DIR=$(pwd)
+
 if [ ! -d "results" ] 
 then
-	echo "Result directory does not exist. Creating ..."
+	echo -e "Result directory does not exist. Creating ...\n\n"
 	mkdir results
 else
-	echo "Result directory exists"
+	echo -e "Result directory exists\n\n"
 fi
 if [ ! -d "build" ] 
 then
-	echo "Build directory does not exist. Creating ..."
+	echo -e "Build directory does not exist. Creating ...\n\n"
 	mkdir build
 else
-	echo "Build directory exists"
+	echo -e "Build directory exists\n\n"
 fi
+
 cd build
-echo "Running cmake and make in $(pwd)"
+
+echo -e "Running cmake and make in $(pwd)\n\n"
+
 cmake ..
 make
-./benchmark_bg_wkt_o2 -t $DIR/results/benchmark_bg_wkt_o2.csv
-./benchmark_bg_wkt_o3 -t $DIR/results/benchmark_bg_wkt_o3.csv
-./benchmark_bg_wkt_unoptimized -t $DIR/results/benchmark_bg_wkt_unoptimized.csv
+
+echo -e "Executing benchmarks\n\n"
+
+if [[ ! -f "$DIR/results/benchmark_bg_wkt_o2.csv" ]] || [[ $1 != "-n" ]]
+then
+	./benchmark_bg_wkt_o2 -t $DIR/results/benchmark_bg_wkt_o2.csv
+else
+	./benchmark_bg_wkt_o2
+fi
+
+
+if [[ ! -f "$DIR/results/benchmark_bg_wkt_o3.csv" ]] || [[ $1 != "-n" ]]
+then
+	./benchmark_bg_wkt_o3 -t $DIR/results/benchmark_bg_wkt_o3.csv
+else
+	./benchmark_bg_wkt_o3
+fi
+
+
+if [[ ! -f "$DIR/results/benchmark_bg_wkt_unoptimized.csv" ]] || [[ $1 != "-n" ]]
+then
+	./benchmark_bg_wkt_unoptimized -t $DIR/results/benchmark_bg_wkt_unoptimized.csv
+else
+	./benchmark_bg_wkt_unoptimized
+fi
